77dd5a697a4523f3cbc242e62227a3fc2053d552
eversonturossi <eversont@gmail.com>
2023-08-17 10:52:10 -0300

    ajuste

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -16,19 +16,19 @@ public class NoteController : Controller
             new Note { ID = 2, User = 2, Message = "ASP.NET is great!", CreatedAt = DateTime.Now.AddDays(-1), UserSent = 1 },
             new Note { ID = 3, User = 1, Message = "Having a good time learning.", CreatedAt = DateTime.Now, UserSent = 1 },
 
-                        new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 },
+            new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 },
             new Note { ID = 2, User = 2, Message = "ASP.NET is great!", CreatedAt = DateTime.Now.AddDays(-1), UserSent = 1 },
             new Note { ID = 3, User = 1, Message = "Having a good time learning.", CreatedAt = DateTime.Now, UserSent = 1 },
 
-                        new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1  , ReadAt = DateTime.Now},
+            new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1  , ReadAt = DateTime.Now},
             new Note { ID = 2, User = 2, Message = "ASP.NET is great!", CreatedAt = DateTime.Now.AddDays(-1), UserSent = 1  , ReadAt = DateTime.Now},
             new Note { ID = 3, User = 1, Message = "Having a good time learning.", CreatedAt = DateTime.Now, UserSent = 1  , ReadAt = DateTime.Now},
 
-                        new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 , ReadAt = DateTime.Now },
+            new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 , ReadAt = DateTime.Now },
             new Note { ID = 2, User = 2, Message = "ASP.NET is great!", CreatedAt = DateTime.Now.AddDays(-1), UserSent = 1 , ReadAt = DateTime.Now },
             new Note { ID = 3, User = 1, Message = "Having a good time learning.", CreatedAt = DateTime.Now, UserSent = 1  , ReadAt = DateTime.Now},
 
-                        new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 , ReadAt = DateTime.Now },
+            new Note { ID = 1, User = 1, Message = "Hello, world!", CreatedAt = DateTime.Now.AddDays(-2), UserSent = 1 , ReadAt = DateTime.Now },
             new Note { ID = 2, User = 2, Message = "ASP.NET is great!", CreatedAt = DateTime.Now.AddDays(-1), UserSent = 1  , ReadAt = DateTime.Now},
             new Note { ID = 3, User = 1, Message = "Having a good time learning.", CreatedAt = DateTime.Now, UserSent = 1 , ReadAt = DateTime.Now}
         };
